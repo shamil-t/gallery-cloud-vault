461bb5236b52f30aa554a9bed21b628cd528d189
shamil-t <tshamil90@gmail.com>
2026-08-07 09:56:00 +0530

    Release v1.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,7 +25,7 @@ android {
         minSdk = 29
         targetSdk = 37
         versionCode = 5
-        versionName = "1.2.3"
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
